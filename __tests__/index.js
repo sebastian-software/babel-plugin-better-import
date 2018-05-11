6a4c71e2394d799097ebc62e96b6f0f9168bbbce
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-11 12:50:35 +0200

    Refactored babel settings to not rely on stage-2 preset

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,51 +1,50 @@
-/* eslint-disable no-template-curly-in-string */
+import pluginTester from "babel-plugin-tester"
+import createBabylonOptions from "babylon-options"
 
-const pluginTester = require('babel-plugin-tester')
-const createBabylonOptions = require('babylon-options')
-const plugin = require('../index')
-const babel = require('@babel/core')
-const dynamicSyntax = require('@babel/plugin-syntax-dynamic-import')
+import plugin from "../index"
 
 const babelOptions = {
-  filename: 'currentFile.js',
+  filename: "currentFile.js",
   parserOpts: createBabylonOptions({
-    stage: 2
+    plugins: [ "dynamicImport" ]
   })
 }
+
+/* eslint-disable no-template-curly-in-string */
 
 pluginTester({
   plugin,
   babelOptions,
   snapshot: true,
   tests: {
-    'static import': 'import("./Foo")',
-    'static import (with relative paths)': 'import("../../Foo")',
-    'static import (with file extension)': 'import("./Foo.js")',
-    'static import (string template)': 'import(`./base`)',
-    'static import (string template + relative paths)': 'import(`../../base`)',
-    'static import (import as function with relative paths + nested folder)':
-      'const obj = {component:()=>import(`../components/nestedComponent`)}; ()=> obj.component()',
-    'static import (relative paths + nested folder)':
-      'import(`../components/nestedComponent`)',
-    'dynamic import (string template)': 'import(`./base/${page}`)',
-    'dynamic import (string template with nested folder)':
-      'import(`./base/${page}/nested/folder`)',
-    'dynamic import (string template with multiple nested folders)':
-      'import(`./base/${page}/nested/{$another}folder`)',
-    'dynamic import (string template - dynamic at 1st segment)':
-      'import(`./${page}`)',
-    'dynamic import (string template + relative paths)':
-      'import(`../../base/${page}`)',
-    'await import() should receive a thennable without calling .then':
-      'async ({ page }) => await import(`../components/${page}`);',
-    'babelServer: true': {
+    "static import": 'import("./Foo")',
+    "static import (with relative paths)": 'import("../../Foo")',
+    "static import (with file extension)": 'import("./Foo.js")',
+    "static import (string template)": "import(`./base`)",
+    "static import (string template + relative paths)": "import(`../../base`)",
+    "static import (import as function with relative paths + nested folder)":
+      "const obj = {component:()=>import(`../components/nestedComponent`)}; ()=> obj.component()",
+    "static import (relative paths + nested folder)":
+      "import(`../components/nestedComponent`)",
+    "dynamic import (string template)": "import(`./base/${page}`)",
+    "dynamic import (string template with nested folder)":
+      "import(`./base/${page}/nested/folder`)",
+    "dynamic import (string template with multiple nested folders)":
+      "import(`./base/${page}/nested/{$another}folder`)",
+    "dynamic import (string template - dynamic at 1st segment)":
+      "import(`./${page}`)",
+    "dynamic import (string template + relative paths)":
+      "import(`../../base/${page}`)",
+    "await import() should receive a thennable without calling .then":
+      "async ({ page }) => await import(`../components/${page}`);",
+    "babelServer: true": {
       code: 'import("./Foo")',
       pluginOptions: { babelServer: true }
     },
-    'disableWarnings: true': {
+    "disableWarnings: true": {
       code: 'import("./Foo")',
       pluginOptions: { disableWarnings: true }
     },
-    'existing chunkName': 'import(/* webpackChunkName: \'Bar\' */"./Foo")'
+    "existing chunkName": "import(/* webpackChunkName: 'Bar' */\"./Foo\")"
   }
 })
