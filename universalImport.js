ae22696772891e3a90f326a6b9ee87abf0bf3638
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-11 13:33:40 +0200

    Removed eslint disable flag

--- universalImport.js
+++ universalImport.js
@@ -1,35 +1,33 @@
-/* eslint-disable */
-
 module.exports = function(config, makeThennable) {
   if (makeThennable === false) return config
 
-  var load = config.load
+  const load = config.load
   config.then = function(cb) {
-    return load().then(function(res) {
+    return load().then((res) => {
       return cb && cb(res)
     })
   }
   config.catch = function(cb) {
-    return load().catch(function(e) {
+    return load().catch((e) => {
       return cb && cb(e)
     })
   }
   return config
 }
 
-var isSet = false
+let isSet = false
 
 function setHasPlugin() {
   if (isSet) return
-  var universal
-  var isWebpack = typeof __webpack_require__ !== 'undefined'
+  let universal
+  const isWebpack = typeof __webpack_require__ !== "undefined"
 
   try {
     if (isWebpack) {
-      var weakId = require.resolveWeak('react-universal-component')
+      const weakId = require.resolveWeak("react-universal-component")
       universal = __webpack_require__(weakId)
     } else {
-      var pkg = 'react-universal-component'
+      const pkg = "react-universal-component"
       universal = module.require(pkg)
     }
 
